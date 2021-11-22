ca8fc1050bcf6eb9dee38b3e062cbd2bbe802039
Darin Evanow <darin.evanow@gusto.com>
2021-11-21 22:19:11 -0800

    Have two pipes rendering with a variable vertical positioning and variable range space between them.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ const config = {
   height: 600,
   physics: {
     default: "arcade", // Arcade physics plugin, manages physics simulation
-    arcade: {
-      gravity: { y: 800 },
-    },
   },
   scene: {
     preload: preload, // Called before the game starts
@@ -18,15 +15,26 @@ const config = {
   },
 };
 
-const FLAP_VELOCITY = 400;
-const initialBirdPosition = { x: config.width / 10, y: config.height / 2 };
-
+// Bird variables
 let bird;
+const initialBirdPosition = { x: config.width / 10, y: config.height / 2 };
+const FLAP_VELOCITY = 400;
+
+// Pipe variables
+const pipeVerticalDistanceRange = [150, 250];
+let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+let pipeVerticalPosition = Phaser.Math.Between(
+  20,
+  config.height - 20 - pipeVerticalDistance
+);
+let upperPipe;
+let lowerPipe;
 
 // Used to load assets such as images, animations, music, etc.
 function preload() {
   this.load.image("sky", "assets/sky.png");
   this.load.image("bird", "assets/bird.png");
+  this.load.image("pipe", "assets/pipe.png");
 }
 
 // Used to set the initial scene, such as adding the background or title screen
@@ -35,6 +43,14 @@ function create() {
   bird = this.physics.add
     .image(initialBirdPosition.x, initialBirdPosition.y, "bird")
     .setOrigin(0);
+  bird.body.gravity.y = 400;
+
+  upperPipe = this.physics.add
+    .sprite(300, pipeVerticalPosition, "pipe")
+    .setOrigin(0, 1);
+  lowerPipe = this.physics.add
+    .sprite(300, upperPipe.y + pipeVerticalDistance, "pipe")
+    .setOrigin(0, 0);
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
